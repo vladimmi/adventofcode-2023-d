516077514a6420c10d6c6144f2b7a909f4dcb49e
vladimmi <vladimmi@gmail.com>
2023-12-07 19:13:55 +0300

    day 7 puzzle 1

---
+++ 7-1/source/app.d
@@ -0,0 +1,60 @@
+import std.algorithm;
+import std.conv;
+import std.format;
+import std.math;
+import std.range;
+import std.stdio;
+import std.string;
+import std.typecons;
+
+
+enum string input = import("input.txt");
+
+enum cards = "23456789TJQKA";
+enum HandType : byte {
+    HighCard,
+    OnePair,
+    TwoPairs,
+    ThreeOfKind,
+    FullHouse,
+    FourOfKind,
+    FiveOfKind
+}
+
+HandType handType(string hand) {
+    auto data = hand.array().sort().group().array().sort!((a, b) => a[1] > b[1]);
+
+    if (data[0][1] == 5) return HandType.FiveOfKind;
+    else if (data[0][1] == 4) return HandType.FourOfKind;
+    else if (data[0][1] == 3 && data[1][1] == 2) return HandType.FullHouse;
+    else if (data[0][1] == 3) return HandType.ThreeOfKind;
+    else if (data[0][1] == 2 && data[1][1] == 2) return HandType.TwoPairs;
+    else if (data[0][1] == 2) return HandType.OnePair;
+    return HandType.HighCard;
+}
+
+void main() {
+    auto lines = input.split("\r\n");
+
+    int result = 0;
+    auto data = lines.map!((line) {
+            auto l = line.split();
+            return tuple(l[0], l[1].to!int);
+        })
+        .array()
+        .sort!((a, b) {
+            if (a[0].handType < b[0].handType) return true;
+            if (a[0].handType > b[0].handType) return false;
+            for (int i = 0; i < a[0].length; i++) {
+                if (cards.countUntil(a[0][i]) < cards.countUntil(b[0][i])) return true;
+                if (cards.countUntil(a[0][i]) > cards.countUntil(b[0][i])) return false;
+            }
+            return false;
+        });
+    
+    for (int i = 0; i < data.length; i++) {
+        result += (i + 1) * data[i][1];
+    }
+
+    writeln("Result: ", result);
+}
